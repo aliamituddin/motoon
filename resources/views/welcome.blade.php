fdc71dac7c621794abf6231958576a560048312d
Karam Qubsi <karamqubsi@gmail.com>
2017-05-19 17:04:37 +0800

    some tests

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,79 +1,41 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>متون : نظام لضبط المرويات الإسلامية</title>
+  <title>متون : نظام لضبط المرويات الإسلامية</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+  <!-- Styles -->
+  <link href="/css/app.css" rel="stylesheet" type="text/css">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-            .full-height {
-                height: 100vh;
-            }
+  <script>
+  window.Laravel = <?php echo json_encode([
+    'csrfToken' => csrf_token(),
+  ]); ?>
+  </script>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+</head>
+<body>
+  <div class="flex-center position-ref full-height">
 
 
-            <div class="content">
-                <div class="title m-b-md">
-                    متون : لضبط وتنظيم المرويات الإسلامية
-                </div>
+    <div class="content">
+      <div class="title m-b-md">
+        متون : لضبط وتنظيم المرويات الإسلامية
+      </div>
 
-            </div>
-        </div>
-    </body>
+    </div>
+  </div>
+
+  <div id="app">
+    <example> </example>
+  </div>
+  <script src="/js/app.js"></script>
+</body>
 </html>
