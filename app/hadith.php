57ee4d1425ee2e7684cc734c083592a72de7f530
Karam Qubsi <karamqubsi@gmail.com>
2017-08-10 15:50:01 +0800

    implement basic elasticsearch using scout

--- app/hadith.php
+++ app/hadith.php
@@ -2,10 +2,12 @@
 
 namespace App;
 
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 
 class hadith extends Model
 {
+      use Searchable;
     public function chapter (){
       return $this->belongsTo(chapter::class);
     }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
 	public function run()
 	{
 	 $this->call(muslim::class);
-	 $this->call(Bukhari::class);
+	//  $this->call(Bukhari::class);
 
 	}
 }
