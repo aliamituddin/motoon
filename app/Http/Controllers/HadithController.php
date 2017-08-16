cb8c151948123c8e5af09a9853aac7312528a373
Karam Qubsi <karamqubsi@gmail.com>
2017-08-16 15:20:35 +0800

    removing some comments

--- app/Http/Controllers/HadithController.php
+++ app/Http/Controllers/HadithController.php
@@ -46,9 +46,6 @@ class HadithController extends Controller
      */
     public function show(hadith $hadith)
     {
-        // $chapter = $hadith->chapter;
-        // $book = $chapter->book;
-        // $source = $book->source;
         return $hadith;
 
     }
@@ -89,6 +86,5 @@ class HadithController extends Controller
     public function search ($search) {
       $result =  \App\hadith::search($search)->paginate();
         return $result;
-        // $orders = App\Order::search('Star Trek')->raw();
     }
 }
